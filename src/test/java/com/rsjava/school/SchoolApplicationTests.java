bbe35566de720a1a0dd87c568f7247d0344c68f0
rsjavasolutions <stan.robert.1986@gmail.com>
2020-06-14 20:12:29 +0200

    New project school relations

---
+++ src/test/java/com/rsjava/school/SchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsjava.school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
