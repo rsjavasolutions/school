2912f75cc53032889b4a2c34cc01f8c35b0fa02c
rsjavasolutions <stan.robert.1986@gmail.com>
2020-06-14 21:54:22 +0200

    Created model class: Cleaner

---
+++ src/main/java/com/rsjava/school/model/Cleaner.java
@@ -0,0 +1,25 @@
+package com.rsjava.school.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "cleaners")
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class Cleaner {
+
+    @NonNull
+    @Column(name = "name")
+    private String name;
+    @NonNull
+    @Column(name = "surname")
+    private String surname;
+}
